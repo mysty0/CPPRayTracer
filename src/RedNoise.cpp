3d45fe282caf4ba72b1196824a1e50928834d761
Yaroslav <maplegend@mail.ru>
2022-10-03 14:57:36 +0100

    fix tabs2

--- src/RedNoise.cpp
+++ src/RedNoise.cpp
@@ -10,62 +10,62 @@
 #define HEIGHT 240
 
 std::vector<float> interpolateSingleFloats(float from, float to, int resolution) {
-	std::vector<float> result(resolution);
-	float step = (to - from)/(resolution-1);
-	for(int i = 0; i < resolution; ++i) {
-		result[i] = from + step * i;
-	}
-	return result;
+    std::vector<float> result(resolution);
+    float step = (to - from)/(resolution-1);
+    for(int i = 0; i < resolution; ++i) {
+        result[i] = from + step * i;
+    }
+    return result;
 }
 
 std::vector<glm::vec3> interpolateThreeElementValues(glm::vec3 from, glm::vec3 to, int resolution) {
-	std::vector<glm::vec3> result(resolution);
-	auto step = (to - from) / glm::vec3(resolution-1);
-	for(int i = 0; i < resolution; ++i) {
-		result[i] = from + step * glm::vec3(i);
-	}
-	return result;
+    std::vector<glm::vec3> result(resolution);
+    auto step = (to - from) / glm::vec3(resolution-1);
+    for(int i = 0; i < resolution; ++i) {
+        result[i] = from + step * glm::vec3(i);
+    }
+    return result;
 }
 
 void draw(DrawingWindow &window) {
-	window.clearPixels();
-	glm::vec3 topLeft(255, 0, 0);        // red 
-	glm::vec3 topRight(0, 0, 255);       // blue 
-	glm::vec3 bottomRight(0, 255, 0);    // green 
-	glm::vec3 bottomLeft(255, 255, 0);   // yellow
+    window.clearPixels();
+    glm::vec3 topLeft(255, 0, 0);        // red 
+    glm::vec3 topRight(0, 0, 255);       // blue 
+    glm::vec3 bottomRight(0, 255, 0);    // green 
+    glm::vec3 bottomLeft(255, 255, 0);   // yellow
 
-	auto left = interpolateThreeElementValues(topLeft, bottomLeft, window.height);
-	auto right = interpolateThreeElementValues(topRight, bottomRight, window.height);
-	for (size_t y = 0; y < window.height; y++) {
-		auto row = interpolateThreeElementValues(left[y], right[y], window.width);
-		for (size_t x = 0; x < window.width; x++) {
-			glm::vec3 col =  row[x];
-			uint32_t color = (255 << 24) + (int(col.r) << 16) + (int(col.g) << 8) + int(col.b);
-		    window.setPixelColour(x, y, color);
-		}
-	}
+    auto left = interpolateThreeElementValues(topLeft, bottomLeft, window.height);
+    auto right = interpolateThreeElementValues(topRight, bottomRight, window.height);
+    for (size_t y = 0; y < window.height; y++) {
+        auto row = interpolateThreeElementValues(left[y], right[y], window.width);
+        for (size_t x = 0; x < window.width; x++) {
+            glm::vec3 col =  row[x];
+            uint32_t color = (255 << 24) + (int(col.r) << 16) + (int(col.g) << 8) + int(col.b);
+            window.setPixelColour(x, y, color);
+        }
+    }
 }
 
 void handleEvent(SDL_Event event, DrawingWindow &window) {
-	if (event.type == SDL_KEYDOWN) {
-		if (event.key.keysym.sym == SDLK_LEFT) std::cout << "LEFT" << std::endl;
-		else if (event.key.keysym.sym == SDLK_RIGHT) std::cout << "RIGHT" << std::endl;
-		else if (event.key.keysym.sym == SDLK_UP) std::cout << "UP" << std::endl;
-		else if (event.key.keysym.sym == SDLK_DOWN) std::cout << "DOWN" << std::endl;
-	} else if (event.type == SDL_MOUSEBUTTONDOWN) {
-		window.savePPM("output.ppm");
-		window.saveBMP("output.bmp");
-	}
+    if (event.type == SDL_KEYDOWN) {
+        if (event.key.keysym.sym == SDLK_LEFT) std::cout << "LEFT" << std::endl;
+        else if (event.key.keysym.sym == SDLK_RIGHT) std::cout << "RIGHT" << std::endl;
+        else if (event.key.keysym.sym == SDLK_UP) std::cout << "UP" << std::endl;
+        else if (event.key.keysym.sym == SDLK_DOWN) std::cout << "DOWN" << std::endl;
+    } else if (event.type == SDL_MOUSEBUTTONDOWN) {
+        window.savePPM("output.ppm");
+        window.saveBMP("output.bmp");
+    }
 }
 
 int main(int argc, char *argv[]) {
-	DrawingWindow window = DrawingWindow(WIDTH, HEIGHT, false);
-	SDL_Event event;
-	while (true) {
-		// We MUST poll for events - otherwise the window will freeze !
-		if (window.pollForInputEvents(event)) handleEvent(event, window);
-		draw(window);
-		// Need to render the frame at the end, or nothing actually gets shown on the screen !
-		window.renderFrame();
-	}
+    DrawingWindow window = DrawingWindow(WIDTH, HEIGHT, false);
+    SDL_Event event;
+    while (true) {
+        // We MUST poll for events - otherwise the window will freeze !
+        if (window.pollForInputEvents(event)) handleEvent(event, window);
+        draw(window);
+        // Need to render the frame at the end, or nothing actually gets shown on the screen !
+        window.renderFrame();
+    }
 }
